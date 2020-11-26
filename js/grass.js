6d85d59480698b5d287d5069f8c595c6e1db4ac8
inesbcrodrigues <ines.bc.rodrigues@gmail.com>
2020-11-26 16:28:48 +0000

    Update grass.js

--- js/grass.js
+++ js/grass.js
@@ -6,9 +6,9 @@ function addGrassPlane(){
     var texture = new THREE.TextureLoader().load("grasstexture.jpg");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(5,5);
+    texture.repeat.set(20,20);
 
-    var geometry = new THREE.PlaneBufferGeometry(200, 200, 100, 100);
+    var geometry = new THREE.PlaneBufferGeometry(1000, 1000, 100, 100);
     var material = new THREE.MeshPhongMaterial({map:texture});
 
     pmesh = new THREE.Mesh(geometry, material);
