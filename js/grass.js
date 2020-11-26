9425ae7e814a8427357523a4cbf0d37aadd60b5f
inesbcrodrigues <ines.bc.rodrigues@gmail.com>
2020-11-26 15:38:30 +0000

    Update grass.js

---
+++ js/grass.js
@@ -0,0 +1,17 @@
+//=======================================================================
+//GRASS
+//=======================================================================
+
+/*function addGrassPlane(){
+    var texture = new THREE.TextureLoader().load("grasstexture.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(5,5);
+
+    var geometry = new THREE.PlaneBufferGeometry(200, 200, 100, 100);
+
+    pmesh = new THREE.Mesh(geometry, texture);
+    pmesh.position.set(0, 0, 0);
+    pmesh.receiveShadow = true;
+    scene.add(pmesh);
+}*/
